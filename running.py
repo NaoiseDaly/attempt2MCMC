5dca55cbb72cf761eb560e7b4ba8de0709a00734
Naoise <naoisedaly101@gmail.com>
2024-11-09 20:49:28 +0000

    delete commented out code

--- running.py
+++ running.py
@@ -4,11 +4,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig( level=logging.INFO)
 
 FULL_CHAIN_ITERATIONS = 10
-# X_0 = np.array([2.5, 2.5])
-# list_of_chains = []
-# for starting_point in [
-#     [-2.5,-2.5],[-2.5,2.5],[2.5,2.5],[2.5,-2.5]  ]:
-#     list_of_chains.append( basic_Gibbs_sampler(starting_point, FULL_CHAIN_ITERATIONS) )
+
 starting_points = np.array([[-2.5,-2.5], [-2.5,2.5],[2.5,2.5],[2.5,-2.5]])
 list_of_chains = [basic_Gibbs_sampler(x_0, FULL_CHAIN_ITERATIONS) for x_0 in starting_points]
 simply_plot_the_chains(list_of_chains)
