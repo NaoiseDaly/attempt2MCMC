2bb11c68b58b88adfea5e260d652c9749e095768
Naoise <naoisedaly101@gmail.com>
2024-11-10 15:20:46 +0000

    tweak and tidy code in basic_Gibbs_sampler
    change plot limits in simply_plot_the_chains

--- functions.py
+++ functions.py
@@ -13,14 +13,16 @@ def basic_Gibbs_sampler(X0, max_t_iterations=10**3, rho = .8):
     #start timing here
     start_time = perf_counter()
 
-    
     chain = np.zeros((max_t_iterations,2))
     x = chain[0] = X0
+    #in rvs() scale means stdev
+    std_dev = np.sqrt(1-rho**2)
+
     for t in range(1, max_t_iterations):
+        #at the start, X_t-1 is stored in the variable x
         #X_t will be what is stored in the variable x after this is finished
-        # order of subcomponents is arbitary here, following numbering given in description
-        #in rvs() scale means stdev
-        std_dev = np.sqrt(1-rho**2)
+        # order of subcomponents is arbitary here, following numbering given in book description
+        
         #theta_1 | theta_-1, y
         x[0] = norm.rvs(loc = rho*x[1], scale = std_dev)
         #theta_2 | theta_-2, y
@@ -41,9 +43,11 @@ def simply_plot_the_chains(chains, fmt_plt="k-"):
     plots 2D chains over time on the same plot
     """
     fig, ax = plt.subplots()
+    ax.set_xlim((-4.1,4.1))
+    ax.set_ylim((-4.1,4.1))
     for chain in chains:
         ax.plot(chain[:,0], chain[:,1], fmt_plt)
-        #plot starting point
+        #plot starting point as a black square
         ax.plot(*chain[0,:], "sk", markersize = 15)
     ax.set_title("")
     plt.show()
